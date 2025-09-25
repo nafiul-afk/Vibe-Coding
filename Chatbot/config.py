8c219e8a12a1bd153420cbc846862862a06234ff
Nafiul Islam <nafiulislam490@gmail.com>
2025-09-25 10:48:24 +0600

    Create config.py

---
+++ Chatbot/config.py
@@ -0,0 +1,7 @@
+# Database configuration
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",         # change this
+    "password": "yourpass", # change this
+    "database": "chatbot_db"
+}
